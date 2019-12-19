b482b3780375d708301f87484fe0953277a2334e
giordano.alvitez@utec.edu.pe <giordano.alvitez@utec.edu.pe>
2019-12-18 21:53:46 -0500

    Lesson 2

--- Lessons-2020/lesson2/fibonacci.cpp
+++ Lessons-2020/lesson2/fibonacci.cpp
@@ -3,12 +3,20 @@
 
 using namespace std;
 
-long long fibonacci(long long n){
+long long fibonacci1(long long n){
+  unordered_map<int,long long> dict;
+  dict[0]=0;
+  dict[1]=1ll;
+  for(int i=2;i<=n;i++)
+    dict[i]=dict[i-1]+dict[i-2];
+  return dict[n];
+}
+long long fibonacci2(long long n){
   if(n==0)
     return 0;
   else if(n==1)
     return 1;
-  return fibonacci(n-1)+fibonacci(n-2);
+  return fibonacci2(n-1)+fibonacci2(n-2);
 }
 
 int main(){
@@ -17,15 +25,8 @@ int main(){
 
   cin>>n;
 
-  unordered_map<int,long long> dict;
-
-  dict[0]=0;
-  dict[1]=1ll;
-  for(int i=2;i<=n;i++)
-    dict[i]=dict[i-1]+dict[i-2];
-
-  cout<<dict[n]<<endl;
-  cout<<fibonacci(n)<<endl;
+  cout<<fibonacci1(n)<<endl;
+  cout<<fibonacci2(n)<<endl;
 
 
   return 0;
